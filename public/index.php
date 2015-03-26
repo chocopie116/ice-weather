f53c12eb7e3a635320070202f5be559c617e3b67
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2015-03-26 21:42:37 +0900

    add contribute link

--- public/index.php
+++ public/index.php
@@ -6,5 +6,13 @@
 <h1>Ice weather </h1>
 <h3>現在の冷凍庫の様子です</h3>
 <img src="img/snapshot_20150326_2117.jpg">
+
+<p>
+ice weatherではテンションのあがるサイトをデザインしてくれるデザイナーさんや
+コードをレビューしてくれるエンジニアを募集しています。
+こういう機能つけてほしい！みたいなご意見があれば<a href="https://github.com/chocopie116/ice-weather/issues">こちら</a>に是非issueを上げてください
+</p>
+<a href="https://github.com/chocopie116/ice-weather">Contribute</a>
+
 </body>
 </html>
