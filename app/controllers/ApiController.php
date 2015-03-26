62b47b77b0b82ce870d53521380591b73b55c33e
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2015-03-27 04:21:43 +0900

    ユーザーがアップする画像フォルダをignoreにした

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -20,11 +20,10 @@ class ApiController extends BaseController
         // 文字列状態から画像リソース化
         $image = imagecreatefromstring($img);
 
-
         //画像として保存（ディレクトリは任意）
         imagesavealpha($image, TRUE); // 透明色の有効
 
-        $path = public_path() . '/img/hoge.png';
+        $path = public_path() . '/img/' . date('YmdHis') . '.png';
 
         imagepng($image , $path);
 
