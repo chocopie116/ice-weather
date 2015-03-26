d28f6c18da075b8c8866501fc74d6c1751a30df2
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2015-03-26 21:50:49 +0900

    add meta tag, ad

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <html>
 <head>
+<meta name="description" content="ice weatherは、あなたの「今日何のアイスを食べようかな？」を7FのアイスクリームBOXまでのぞきにいかなくても自席でワクワクできるサービスです">
+<meta name="keywords" content="アイスクリーム、アイス管理人様には足を向けてねれません">
 <title>ice weather</title>
 </head>
 <body>
 <h1>Ice weather </h1>
+<p>Ice weatherは、今日何のアイスを食べようかな？とワクワクしながら7FのアイスクリームBOXまでのぞきにいかなくても自席で確認できるアプリケーションです</p><br>
 <h3>現在の冷凍庫の様子です</h3>
 <img src="img/snapshot_20150326_2117.jpg">
 
@@ -14,5 +17,8 @@ ice weatherではテンションのあがるサイトをデザインしてくれ
 </p>
 <a href="https://github.com/chocopie116/ice-weather">Contribute</a>
 
+<script>
+console.log('ice creamに目がないフロントエンドエンジニア募集中 是非ここからjoinしてください。https://github.com/chocopie116/ice-weather');
+</script>
 </body>
 </html>
