c4ab9563730ccd8fe7d969149cf555eb54fa08f0
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2015-03-27 13:28:01 +0900

    debug

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -4,15 +4,31 @@ class ApiController extends BaseController
 {
     public function getImage($target)
     {
+        $this->postImage($target);
     }
 
     public function postImage($target)
     {
         $img = Input::get('img');
 
-        $imagePublicPath = '/img/ice/' . date('YmdHis') . '.png';
-        $filePath = public_path() . $imagePublicPath;
-        move_uploaded_file($_FILES['acceptImage']['tmp_name'], $filePath);
+        //$imagePublicPath = '/img/ice/' . date('YmdHis') . '.png';
+        //$filePath = public_path() . $imagePublicPath;
+        //move_uploaded_file($_FILES['acceptImage']['tmp_name'], $filePath);
+
+        //TODO secretなので絶対commitすんな
+        \Cloudinary::config(array(
+            "cloud_name" => "hfjbiwaqs",
+            "api_key" => "189555235614418",
+            "api_secret" => "nkaPz4jviODAo0yVbFJYuYmg_9o"
+        ));
+        //cloudinary://189555235614418:nkaPz4jviODAo0yVbFJYuYmg_9o@hfjbiwaqs
+
+        //var_dump($_FILES['acceptImage']);
+        echo 'ok';
+        $file = '/home/demouser/ice-weather/public/img/snapshot_20150326_2117.jpg';
+        //$result = \Cloudinary\Uploader::upload($_FILES['acceptImage']['tmp_name']);
+        $result = \Cloudinary\Uploader::upload($file);
+        var_dump($result);
 
         //DBに保存
 
